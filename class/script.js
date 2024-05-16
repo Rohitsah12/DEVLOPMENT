b2044dc620bbd808feae106b9b63160227b62e9f
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-16 17:34:44 +0530

    first commit

---
+++ class/script.js
@@ -0,0 +1,12 @@
+console.log(rohit);
+sum();
+let rohit="ioi";
+function sum(){
+    console.log("Hello")
+}
+
+
+
+//Output
+//undefined
+//Hello
---
+++ javascript/lec01.js
@@ -0,0 +1,61 @@
+//variable
+// var name1="IOI";
+// let name2="IOI2";
+// const name3="IOI3";
+
+// console.log(name3);
+//Datatypes
+//Number=> int,float,double
+//String=>Char,Combination of character
+//Boolean=>the word boolean come from->George Boole True or False
+//undefined=> when the variable is declared but the value is not assigned
+//Null=> when there is no value
+
+// let loan_num=null;
+// loan_num=3;
+// loan_num=5;
+//Array=> [],=> key is index
+//Object=> {}=> key is of our own choice
+
+//Operator
+//Value always travel from left to right
+//Assignment operartor
+//Arithmetic operator => +,-,*,/,%
+//relational
+//logical=> &&,||
+//Relational=> >,>........
+//Bitwise Hold
+
+//Conditions
+//always use 
+// if(){
+//     block true
+// }
+// if(condition){
+
+// }
+// else if(){
+
+// }
+// else{
+
+// }
+// Switch
+// switch(condition){
+//     case 1:{
+//         break;  
+//     }
+// }
+//Ternary op:
+//Cond?true:false=>login
+// ?:?:=> Topic to research
+//
+
+Num=30;
+30?console.log("Rohit"):console.log("vijay");
+
+
+
+
+
+
