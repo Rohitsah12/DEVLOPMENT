41f5df6d0a222656783692c18ca55a63b6a3d702
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-17 18:22:46 +0530

    java script

---
+++ javascript/lec02.js
@@ -0,0 +1,19 @@
+//self invocation function is also known as IIft
+//ARROY function
+// let name=function(){
+//     console.log("ROhit");
+// };
+
+// name();
+// let sum=function( a, b){
+//     console.log(a+b);
+// };
+// sum(10,20)
+
+let person = {
+    firstName: "Rohit",
+    lastName: "sah",
+    age: 19,
+    phnno: 0330393,
+  };
+
