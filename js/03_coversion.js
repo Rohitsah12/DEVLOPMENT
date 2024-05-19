d40c865248c35d8da4f002aa5848211bf51a6c1a
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-19 10:52:57 +0530

    conversion

---
+++ js/03_coversion.js
@@ -0,0 +1,19 @@
+// let score =true
+// console.log(typeof(score));
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+//"33"=> 33
+//"33abc"=>NaN
+//true=> 1; false=> 0
+
+// let isloggedIn=1
+// let booleanIsLoggedIn=Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+
+let sumeNumber=33
+let stringNumber=String(sumeNumber);
+console.log(stringNumber);
+
+
