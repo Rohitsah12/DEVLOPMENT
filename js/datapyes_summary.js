8b519fa2b7ea53c0b77b80d6444b56c167cef6bf
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-19 11:27:56 +0530

    data types

---
+++ js/datapyes_summary.js
@@ -0,0 +1,30 @@
+//primitive data types
+//7 types : string, Number , bOOlean, null, undefined,symbol
+
+// const score=100
+// const scoreValue=100.3
+
+// const isLoggedIn=false
+// const outsideTemp=null
+// let userEmail;
+
+// const id=Symbol('123')
+// const anotherId=Symbol('123');
+// console.log(id === anotherId);
+
+
+
+
+//Reference(Non primitive)
+//Array,objects,functions
+
+const heros=["shaktiman","naagraj","doga"]
+let myObj={
+    name: "hitesh",
+    age: 22,
+}
+
+const myfunction=function(){
+    console.log("hello world");
+}
+
