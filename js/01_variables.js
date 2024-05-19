9a7be7c7f47bca4f06db1ddb00e36b25837a0170
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-19 10:39:54 +0530

    data types

---
+++ js/01_variables.js
@@ -0,0 +1,18 @@
+const accountID = 144553
+let accountEmail= "rojhr"
+var acountPassword="12345"
+accountCity="Jaipur"
+
+// accountID=2//Not allowed
+accountEmail="hc"
+acountPassword="212322"
+accountCity="Bengalure"
+console.log(accountID);
+
+/*
+prefer Not to use var
+because of issue in block scope and functional scope 
+*/
+
+
+console.table([accountID,accountEmail,acountPassword,accountCity])
---
+++ js/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3)//we are using nodejs, not browser
+
+//code readability should be high
+let name="hitesh"// string
+let age=18 // number
+let isloggedIn=true//boolean
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>value not assigned till now
+//Symbol=> unique
+
+
+//object
+console.log(typeof undefined);
