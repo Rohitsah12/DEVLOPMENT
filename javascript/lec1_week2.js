e5874b9059ca2fc8bb66c8ff635db65b8b8d2c68
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-05-20 11:25:56 +0530

    Higher order function

---
+++ javascript/lec1_week2.js
@@ -0,0 +1,20 @@
+// const callback = (n)=>{
+//     return n ** 2;
+// };
+
+// function cube(callback,n){
+//     return callback(n)*n;
+// };
+// console.log(cube(callback,3));
+
+// function gret(){
+//     console.log("Hello");
+// }
+// setInterval(gret,1000)
+// setInterval(() => {
+//     console.log("Hello");
+// }, 10000);
+
+// setTimeout(() => {
+//     console.log("Hello")
+// }, 5000);
